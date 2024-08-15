5de7c1daeebe24bb76ce8a9dec7bb2a25fdebc9c
CarwilB <54286746+CarwilB@users.noreply.github.com>
2024-08-15 09:02:18 -0700

    New function

---
+++ R/translated_levels.R
@@ -0,0 +1,27 @@
+#' Translated levels
+#'
+#' Translate the levels vector for a categorical factor.
+#'
+#' @param levels_vector One-dimensional vector of levels of a factor
+#' @param variable The variable involved, passed as a string.
+#' @param variables A list of variables in c("firstvar", "secondvar") format.
+#' @param translation_table Consolidated translation table to be used
+#'   (translation_table$variable should be a translation table for
+#'   the variable involved).
+#' @param dest_lang Language to be translated to, typically as a short code.
+#' @param source_lang Language to be translated from, typically as a short code.
+#'
+#' @returns A one-dimensional levels vector for the factor.
+#'
+#' @examples
+#' sr_levels_es <- sr_levels %>% translated_levels("state_responsibility")
+translated_levels <- function(levels_vector, variable, translation_table,
+                              dest_lang="es", source_lang="en") {
+
+  levels_tibble <- as_tibble(levels_vector)
+  colnames(levels_tibble) <- c(variable)
+  levels_tibble <- levels_tibble %>%
+    translated_join(translation, variable, dest_lang, source_lang)
+  levels_vector_final <- pluck(levels_tibble, paste0(variable, "_", dest_lang))
+  return(levels_vector_final)
+}
