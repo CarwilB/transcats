7a11db6ca4dc18d4b2b61b5132a918c0b1b2e012
CarwilB <54286746+CarwilB@users.noreply.github.com>
2024-08-21 06:32:45 -0700

    include gss_var_table in data.R

--- R/data.R
+++ R/data.R
@@ -38,3 +38,18 @@
 #'
 #' @source <https://ultimateconsequences.github.io/>
 "uc_var_table"
+
+#' Variable name table for gss_cat
+#'
+#' A variable name translation table for the variable gss_cat,
+#' a data source from the General Social Survey that is included
+#' in the package `forcats` and used for examples in `transcats`.
+#'
+#' @format ## `gss_var_table`
+#' A data frame with 4 rows and 10 columns. The rows correspond to
+#' English, Spanish, French and the r_variable. The final row matches the
+#' names of the columns. Rows are indexted by language codes in the variable
+#' language.
+#'
+#' @source Created as part of `transcats`
+"gss_var_table"
