f23c6eafc2654efffb2e2d2a5699398778742033
CarwilB <54286746+CarwilB@users.noreply.github.com>
2025-07-23 10:54:25 -0700

    New function which_translation_available

--- R/translation-available.R
+++ R/translation-available.R
@@ -112,4 +112,62 @@ which_translation_unavailable_for <- function(dataframe, variable_name,
   return(unavailable_values)
 }
 
+#' Show values with available translations
+#'
+#' @description
+#' `which_translation_available` returns the values for which
+#'  there is an available translation in the translation table.
+#'
+#'  Note that unlike functions like `translation_available_for`,
+#'  `which_translation_available` does not check the values
+#'  within a dataframe or vector, but looks directly at the
+#'  translation table and returns all possible values that
+#'  can be translated.
+#'
+#' @param variable_name The name of the variable to be checked,
+#'  as a string.
+#' @param translation_table A translation table, by default
+#'   this is the active translation table.
+#' @param dest_lang The destination language code, by default
+#'  this is the current source language, set by set_dest_lang().
+#' @param source_lang The source language code, by default
+#'  this is the current source language, set by set_source_lang().
+#' @param verbose If TRUE, prints messages about the process.
+#'
+#' @returns A character vector with the values that have translations
+#'  available.
+#'
+#' @export
+which_translation_available <-  function(variable_name,
+                                         translation_table = tcats$translation_table,
+                                         dest_lang = tcats$dest_lang,
+                                         source_lang = tcats$source_lang,
+                                         verbose = FALSE) {
+  if (!is.character(variable_name) || length(variable_name) != 1) {
+    stop("The variable name must be a single string.")
+  }
+  if (!(variable_name %in% names(translation_table))) {
+    if (verbose) message("Variable not found in translation table.")
+    return(NULL)
+  }
+
+  var_trans_table <- purrr::pluck(translation_table, variable_name)
+  var_trans_table <- var_trans_table [ , names(var_trans_table) %in%
+                                         c(source_lang, dest_lang)]
+
+  if (!(source_lang %in% names(var_trans_table))){
+    if (verbose) message("Source language not found in translation table.")
+    return(NULL)
+  }
+  if (!(dest_lang %in% names(var_trans_table))){
+    if (verbose) message("Destination language not found in translation table.")
+    return(NULL)
+  }
+  # Get the values that have translations available
+  filtered_var_trans_table <- var_trans_table %>%
+    filter((.[[1]] != "") & !is.na(.[[1]])) %>%
+    filter((.[[2]] != "") & !is.na(.[[2]]))
+  filtered_var_trans_table[[1]] %>%
+    unique() %>%
+    sort()
 }
