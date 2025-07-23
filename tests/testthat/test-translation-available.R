90d070bc6e03c5b82c770bd7bc20d52eb27e4864
CarwilB <54286746+CarwilB@users.noreply.github.com>
2025-07-23 10:54:48 -0700

    + test

--- tests/testthat/test-translation-available.R
+++ tests/testthat/test-translation-available.R
@@ -26,3 +26,20 @@ test_that("Testing translation_available functions", {
   expect_equal(translation_available_for(forcats::gss_cat, "marital"),
                NULL)
 })
+test_that("Testing which_translation_available", {
+  set_active_translation_table(gss_translation)
+  set_source_lang("en")
+  set_dest_lang("fr")
+  expect_equal(which_translation_available("marital"),
+               sort(c("Never married", "Divorced", "Widowed", "Married", "Separated",
+                 "No answer")))
+  expect_equal(which_translation_available("partyid"),
+               sort(c("Ind,near rep", "Not str republican", "Independent",
+                 "Not str democrat", "Strong democrat", "Ind,near dem",
+                 "Strong republican", "Other party", "No answer", "Don't know")))
+  set_dest_lang("cz") # Czech is not available
+  expect_equal(which_translation_available("marital"),
+               NULL)
+  expect_equal(which_translation_available("partyid"),
+               NULL)
+})
